64f6536112472df79abe48fa1267f6d7dd1881c2
natalietsvia-sys <natalietsvia@gmail.com>
2026-05-01 20:51:14 +0300

    Create Truck.java

---
+++ Truck.java
@@ -0,0 +1,14 @@
+public class Truck extends Car {
+    private double towingCapacity;
+
+    public Truck(String vinNumber, String make, String model, int mileage, double towingCapacity) {
+        super(vinNumber, make, model, mileage);
+        this.towingCapacity = towingCapacity;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() +
+                ", Towing capacity: " + towingCapacity + " lbs";
+    }
+}
